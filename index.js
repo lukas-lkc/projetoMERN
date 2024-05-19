118bdee941f0fa55d0d1cffc5275a9a92509e30f
lukas-lkc <luccascarpi@gmail.com>
2024-05-19 19:36:51 -0300

    Dotenv import

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import connectDatabase from "./src/database/db.js";
 import userRoute from "./src/routes/user.route.js";
+import dotenv from "dotenv"
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; //process.env.PORT É o servidor que define a porta de forma automática. Ou 3000 pq por enquanto pra mim vai rodar na 3000
 
 connectDatabase();
 app.use(express.json()); //deixa a aplicação apta a receber e enviar arquivos json
-
 
 app.use("/user", userRoute);
 
--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,7 @@
 import mongoose from 'mongoose';
 
-const mongoURI = 'mongodb+srv://root:root@cluster0.c20idd6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-
 const connectDatabase = () => {
-    /* mongoose.connect("")
-    const connect = mongoose.connection;
-    connect.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    connect.once('open', ()=> {
-        console.log('Connected to MongoDB Atlas');
-    }) */
-    mongoose.connect(mongoURI).then(() => {
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log('Connected to MongoDB Atlas');
     }).catch((err) => {
         console.error('MongoDB Atlas connection error:', err);

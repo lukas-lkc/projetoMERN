0624e2826a8c96f6a3f170d167d755e5a554c32a
lukas-lkc <luccascarpi@gmail.com>
2024-05-26 19:17:09 -0300

    Pagination

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js"
+import { createService, findAllService, countNews } from "../services/news.service.js"
 
 const create = async (req, res) => {
     try {
@@ -22,12 +22,63 @@ const create = async (req, res) => {
     }
 };
 
+//função que tras todos os posts, com paginação (limitando os retornos para garantir performance do db)
 const findAll = async (req, res) => {
-    const news = await findAllService();
+
+    //para limitar a quantidade de resultados de uma chamada.
+    //essas duas variáveis serão parâmetros na URL
+    let {limit, offset} = req.query;
+    //tudo que vem da query/URL vem como string, então aqui transforma em número. 
+    limit = Number(limit);
+    offset = Number(offset);
+    //se o limit não for informado, por padrão será 5
+    if(!limit){
+        limit = 5;
+    }
+    if(!offset){
+        offset = 0;
+    }
+    console.log(limit, offset)
+
+    //a função findAllService está trazendo todos os posts. Mas se forem milhões de dados vai ficar muito pesado
+    //Por isso foram criados o limit e offset que serão passados como parâmetros da função
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    //pega a URL padrão
+    const currentUrl = req.baseUrl;
+    console.log("url padrão: ", currentUrl);
+    console.log("total: ",total);
+
+    const nextPosts = offset + limit;
+
+    const nextUrl = nextPosts < total ? `${currentUrl}?limit=${limit}&offset=${nextPosts}`: null;
+    console.log("url: ",nextUrl);
+    
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}`: null;
+
     if (news.length === 0) {
         return res.status(400).send({ message: "There are no registered news" })
     }
-    res.send( news );
+    res.send( {
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+        //map retorna um array com os valores de news, ou seja, cada post 
+        results : news.map(item => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            userName: item.user.username,
+            userAvatar: item.user.avatar, 
+        }))
+    } );
 };
 
 export { 
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,10 +1,17 @@
 import News from "../models/News.js";
 
 const createService = (body) => News.create(body);
+//find busca por todas as postagens. sort _id -1, faz com que a última postagem seja apresentada em primeiro lugar
+//último a chegar, primeiro a sair
+//skip recebe o valor de offset indica que começa no 0, traz o número de limit a partir do elemento do indice 0. após a primeira pesquisa, precisa trocar o indice de 0 para 5, por exemplo, daí pega o 6° elemento em diante 
+//limit é o número de dados que serão trazidos do banco de dados por vez.
+//sem o populate, o user era referenciado apenas com o ID. com o populate, todos os dados do user são trazidos
+const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
 
-const findAllService = () => News.find();
+const countNews = () => News.countDocuments();
 
 export {
     createService, 
-    findAllService
+    findAllService,
+    countNews
 }
